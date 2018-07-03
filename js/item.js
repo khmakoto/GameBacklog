be6a0c34f733351b346f3b3a03fb8f0707d6bee3
Humberto Makoto Morimoto Burgos <humbertomakotomorimoto@gmail.com>
2018-07-03 18:31:09 -0500

    Adding initial js functionality of 'Edit Information' button (toggling info editing on/off with respective css changes in both inputs and button).

--- js/item.js
+++ js/item.js
@@ -1,2 +1,29 @@
 $(document).ready(function() {
+    // Variable that tracks if button should habilitate editing or save information.
+    var editing = false;
+
+    // Function to edit information.
+    $("#editButton").on("click", function() {
+        // If editing, save information.
+        if (editing) {
+            editing = false;
+            $(".input").each(function() {
+                $(this).prop("disabled", true);
+            });
+            $("#editButton").removeClass("saveInformation");
+            $("#editButton").addClass("habilitateEditing");
+            $("#editButton").val("Edit Information");
+            alert("Info saved.");
+        }
+        // If not, habilitate editing.
+        else {
+            editing = true;
+            $(".input").each(function() {
+                $(this).prop("disabled", false);
+            });
+            $(this).removeClass("habilitateEditing");
+            $(this).addClass("saveInformation");
+            $("#editButton").val("Save Information");
+        }
+    });
 });
