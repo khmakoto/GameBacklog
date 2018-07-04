35a83f05e8cbe85ab593a38f88dcee259de61ab4
Humberto Makoto Morimoto Burgos <humbertomakotomorimoto@gmail.com>
2018-07-03 20:02:52 -0500

    Adding platform toogling on/off by clicking on platform logo in item.html

--- js/item.js
+++ js/item.js
@@ -75,7 +75,7 @@ $(document).ready(function() {
         }
     });
 
-    // Variable that tracks if button should habilitate editing or save information.
+    // Variable that tracks if button should enable editing or save information.
     var editing = false;
 
     // Function to edit information.
@@ -83,23 +83,42 @@ $(document).ready(function() {
         // If editing, save information.
         if (editing) {
             editing = false;
+
             $(".input").each(function() {
                 $(this).prop("disabled", true);
             });
+
+            $(".platform").css("cursor", "default");
+            $(".platform").on("click", function() {});
+
             $("#editButton").removeClass("saveInformation");
-            $("#editButton").addClass("habilitateEditing");
+            $("#editButton").addClass("enableEditing");
             $("#editButton").val("Edit Information");
             alert("Info saved.");
         }
-        // If not, habilitate editing.
+        // If not, enable editing.
         else {
             editing = true;
+
             $(".input").each(function() {
                 $(this).prop("disabled", false);
             });
-            $(this).removeClass("habilitateEditing");
+
+            // Platform toogle on/off by click is enabled.
+            $(".platform").css("cursor", "pointer");
+            $(".platform").on("click", function() {
+                if ($(this).hasClass("active")) {
+                    $(this).removeClass("active");
+                }
+                else {
+                    $(this).addClass("active");
+                }
+            });
+
+            $(this).removeClass("enableEditing");
             $(this).addClass("saveInformation");
             $("#editButton").val("Save Information");
         }
     });
+
 });
